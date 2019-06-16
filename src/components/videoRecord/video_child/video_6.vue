62e271cbd304781206642c41e913f6f44e6dbcce
葉靖 <jing@yejingde-MacBook-Pro.local>
2019-06-16 23:35:07 +0800

    fix vedio

--- src/components/videoRecord/video_child/video_6.vue
+++ src/components/videoRecord/video_child/video_6.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="videoContent">
-    <h1>布農族 臺東初來部落打耳祭、收穫祭</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/LiEllbZ72WM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <h1>邵族 逐鹿市集常態演出、鞦韆祭Lhalhaushin、Mashtatun祭儀性杵音</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/i5ZjGWui2bA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <h1>噶瑪蘭族 立德部落豐年祭</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/a2LNXJChLgM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <h1>賽德克族 賽德克巴蘭社青年舞團常態練習</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/KxF61V5Qx1M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h1>「2018撒奇萊雅族火神祭Sakizaya palamal」－桃園市撒奇萊雅協會</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/Xy_c2OKwCwE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h1>「2018撒奇萊雅族火神祭Sakizaya palamal」－撒奇萊雅族人</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/9eQmfNOyVhA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h1>「秀林鄉2018 Mgay Bari太魯閣族感恩祭儀文化活動」</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/1bEt2-db0As" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </template>
 <style scoped>
